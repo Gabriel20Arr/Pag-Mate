cefa1efdf3a1f3d2a24f2947e870aafe12bc9cff
Gabriel <gabriel2021arr@gmail.com>
2023-11-14 22:04:51 -0300

    Maqueta de registraese terminada

--- src/Views/Registrar/Registrarse.jsx
+++ src/Views/Registrar/Registrarse.jsx
@@ -2,15 +2,34 @@ import React from 'react'
 
 import Styles from "./Registrarse.module.css"
 
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export const Registrarse = () => {
+  const navigate = useNavigate()
+
+  const isSubmit = () => {
+    navigate("/")
+  }
+  
   return (
-    <div className={Styles.Contenedor}>
-        <Link to={"/login"}>
-            Login
-        </Link>
-        <h1>Content registrarse</h1>
+    <div className={Styles.Container}>
+      <div className={Styles.ContainerForm}>
+        <form>
+            <h2>Registrarse</h2>
+
+            <label>Email</label>
+            <input placeholder='Enter your Email'></input>
+
+            <label>Password</label>
+            <input placeholder='Enter your Password'></input>
+
+            <Link to={'/login'} className={Styles.Registrar}>
+              <span >You do have an account?</span>
+            </Link>
+            
+            <button onClick={isSubmit}>Enter</button>
+        </form>
+    </div>
     </div>
   )
 }
