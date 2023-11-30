715fd9330412ca7ba9379d3950bd1c0c785cb629
Gabriel <gabriel2021arr@gmail.com>
2023-11-30 20:47:21 -0300

    Cambios de estilos

--- src/Components/Mate/Mate.jsx
+++ src/Components/Mate/Mate.jsx
@@ -4,22 +4,21 @@ import styles from "./Mate.module.css"
 import { Productos }  from "../../dataProduct"
 
 export const Mate = () => {
-  const [rotate, setRotate] = useState(false)
+  const [rotate, setRotate] = useState(false);
 
   const toggleRotate = () => {
-    // setRotate(!rotate)
-    alert('HOLA MUNDO')
+    setRotate(!rotate)
   }
   
   const dataMate = Productos[1];
 
   return (
     <div className={styles.Cmate}> 
-    {/* // <div className={`${styles.Cmate} ${rotate ? styles.rotacion : ""}`}  onclick={toggleRotate}>  */}
-      <div className={styles.Cmate2}>
-       {/* <button onClick={toggleRotate}>HOLA</button> */}
+       
+    <div className={styles.Cmate2}>
        {dataMate.map((producto, index) => (
-         <div key={index} className={styles.container} onclick={toggleRotate}>
+         <div key={index} className={styles.container}>
+
            <h2 className={styles.span0}>{producto.Title}</h2>
  
            <div className={styles.imagenContainer}>
@@ -35,9 +34,6 @@ export const Mate = () => {
                   />
             </div>
             
- 
-           {/* <p className={styles.span2}>{producto.Description}</p> */}
-           
            <p>Precio: {producto.Price}</p>
         
            { 
@@ -45,16 +41,33 @@ export const Mate = () => {
               : 
             <p>Altura: {producto.Altura} cm</p>
            }
+
+
+           {/* //? description */ }
+           {rotate ? '' : <p 
+            // className={styles.span2}
+            className={`${styles.span2 } ${!rotate ? styles.rotacion : styles.rotacion2}`}
+           >
+
+             {producto.Description}
+           
+           </p>}
+           
+           <button 
+              className={styles.btn}
+              onClick={toggleRotate}
+           >  
+            Ver mas..
+           </button>
+           
+
          </div>
       ))}
-     </div>
+      </div>
 
      {/* <div className={styles.Rotate}>
-
        <p className={styles.RContent}>{Productos[1].Description}</p>
-     
      </div> */}
-
 
     </div>
   )
